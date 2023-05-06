cebca71293ea0ff02ce6a696b4e64831438f84ed
时前未 <shiqianwei@freedotech.com>
2023-05-06 09:36:56 +0800

    update

--- update_action.sh
+++ update_action.sh
@@ -32,6 +32,7 @@ https://raw.githubusercontent.com/Cats-Team/AdRules/main/hosts.txt
 https://raw.githubusercontent.com/jdlingyu/ad-wars/master/sha_ad_hosts
 https://raw.githubusercontent.com/ilpl/ad-hosts/master/hosts
 https://raw.githubusercontent.com/neodevpro/neodevhost/master/lite_host
+https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts
 EOF
 #https://raw.githubusercontent.com/neodevpro/neodevhost/master/lite_host
 #https://raw.githubusercontent.com/Goooler/1024_hosts/master/hosts
